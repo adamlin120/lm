45f3ff1747cb4920f33f1a219e84237b0887c88f
adam <adamlin120@gmail.com>
2020-07-10 00:22:56 +0800

    remove unused code

--- utils.py
+++ utils.py
@@ -57,12 +57,3 @@ class AttrDict(dict):
     def __init__(self, *args, **kwargs):
         super(AttrDict, self).__init__(*args, **kwargs)
         self.__dict__ = self
-
-
-def make_logdir(model_name: str):
-    """Create unique path to save results and checkpoints, e.g. runs/Sep22_19-45-59_gpu-7_gpt2"""
-    # Code copied from ignite repo
-    current_time = datetime.now().strftime('%b%d_%H-%M-%S')
-    logdir = os.path.join(
-        'runs', current_time + '_' + socket.gethostname() + '_' + model_name)
-    return logdir
