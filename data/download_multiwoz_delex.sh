aaf0ed23706805cfd0c896a7154b8a02d3098aea
adam <adamlin120@gmail.com>
2020-08-09 15:53:48 +0800

    add prepare_data.sh

--- data/download_multiwoz_delex.sh
+++ data/download_multiwoz_delex.sh
@@ -1,3 +1,3 @@
 wget https://ytlin.s3-ap-northeast-1.amazonaws.com/data/multiwoz2.1_nadst.zip
-unzip multiwoz2.1_nadst.zip -d multiwoz2.1/
+unzip multiwoz2.1_nadst.zip
 rm multiwoz2.1_nadst.zip
--- data/download_sgd.sh
+++ data/download_sgd.sh
@@ -1,3 +1,3 @@
 wget https://ytlin.s3-ap-northeast-1.amazonaws.com/data/sgd_mtk.zip
-unzip sgd_mtk.zip -d sgd/
+unzip sgd_mtk.zip
 rm sgd_mtk.zip
