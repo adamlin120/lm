0363f60173c68b9bbcafe55dbee66e8bde94459b
adam <adamlin120@gmail.com>
2020-08-09 15:40:11 +0800

    add auto download scripts for multiwoz sgd

---
+++ data/download_multiwoz_delex.sh
@@ -0,0 +1,3 @@
+wget https://ytlin.s3-ap-northeast-1.amazonaws.com/data/multiwoz2.1_nadst.zip
+unzip multiwoz2.1_nadst.zip -d multiwoz2.1/
+rm multiwoz2.1_nadst.zip
---
+++ data/download_sgd.sh
@@ -0,0 +1,3 @@
+wget https://ytlin.s3-ap-northeast-1.amazonaws.com/data/sgd_mtk.zip
+unzip sgd_mtk.zip -d sgd/
+rm sgd_mtk.zip
