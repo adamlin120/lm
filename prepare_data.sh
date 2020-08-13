eb1ca8a45f675e9f5e291c6019842647ea4d54ef
adam <adamlin120@gmail.com>
2020-08-14 01:28:20 +0800

    add dataset: Taskmaster

--- prepare_data.sh
+++ prepare_data.sh
@@ -3,6 +3,7 @@ bash download_dailydialog.sh
 bash download_multiwoz_delex.sh
 bash download_personachat.sh
 bash download_sgd.sh
+bash download_taskmaster.sh
 python preprocess_dailydialog.py
 python preprocess_multiwoz.py
 python preprocess_persona-chat.py
